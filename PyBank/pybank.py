c0fbf8b96c522214f5a0b464533f615570b2fb74
Scott Cerny <scottcerny@yahoo.com>
2021-04-01 11:20:49 -0700

    calculated the number of months and started creating ouput table but not to a file yet

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -2,16 +2,55 @@ import os
 import csv
 
 # Set path for file
-csvpath = os.path.join("..", "Resources",  "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
 
-# Open the CSV
-with open(csvpath) as csvfile:
+# create places to store data
+date = []
+profloss = []
+
+
+# with open(udemy_csv, newline='', encoding='utf-8') as csvfile:
+with open(csvpath, newline='', encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    csv_header = next(csvreader)
+#    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header and append to list
+    for row in csvreader:
+    	date.append(row[0])
+#    	print(row[0])
+    	profloss.append(row[1])
+#    	print(row[1])
+
+#    print(date)
+#    print(profloss)
+#    print(len(date))
+
+
+print(f" ")
+print(f" ")
+print(f" ")
+print(f"Financial Analysis")
+print(f"-------------------------------")
+print(f"Total Months: {len(date)}")
+
+
+#    	date.append(row[0])
+#    	profloss.append(row[1])
+
+#cleaned_csv = zip(date, profloss)
+
+#print(cleaned_csv)
+
+#for row in cleaned_csv:
+#	print(row)
 
 
 
 
+#rows = len(group1)
+#columns = len(group1[0])
 
 #calculate total number of months
 
