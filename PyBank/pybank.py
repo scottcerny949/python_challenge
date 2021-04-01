1d5a842c0a8b6f243e167b1b18a3fcb7ff268ecb
Scott Cerny <scottcerny@yahoo.com>
2021-04-01 10:18:59 -0700

    added file import to pybank and created analysis folders

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -1,2 +1,31 @@
 import os
 import csv
+
+# Set path for file
+csvpath = os.path.join("..", "Resources",  "budget_data.csv")
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+
+
+
+
+
+#calculate total number of months
+
+
+#calculate total amount of profit/loss
+
+
+#calculate average change
+
+
+#calculate greatest increase
+
+
+#calculate greatest decrease
+
+
+#export to text file in Analysis folder
