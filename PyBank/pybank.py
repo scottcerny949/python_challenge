fbfcb5cdce9337e10923580cd261de6e51e9852f
Scott Cerny <scottcerny@yahoo.com>
2021-04-05 12:43:23 -0700

    formatting cleanup and added code to write output to a text file

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -6,41 +6,43 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 
 # create lists to store data
 # set months to zero to start
-
 months = 0
 date = []
 profloss = []
-
 
 # read in the csv file
 with open(csvpath, newline='', encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     csv_header = next(csvreader)
-#    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
-# Read each row of data after the header and append to list while incrementing the month count
+# read each row of data after the header and append to list while incrementing the month count
     for row in csvreader:
         months += 1
         #print(row)
         date.append(row[0])
         profloss.append(float(row[1]))
         
+#for debugging purposes
 #print(date)
 #print(profloss)
+
 #set totalprof to the first profloss number
 totalprof = profloss[0]
+
 #set variables to zero
 totalchange = 0
 highest = 0
 lowest = 0
 
+#for debugging purposes
 #print(profloss[0])
 #print(f" total prof:  {totalprof}")
 
 #start at 1 since we already grabbed 0 to determine the change each month
-
 for n in range (1, months):
+
     #calculate total amount of profit/loss
     totalprof += profloss[n]
     
@@ -52,25 +54,35 @@ for n in range (1, months):
     if change > highest:
         highest = change
         month_h = date[n]
+
     #calculate greatest decrease    
     elif change < lowest:
         lowest = change
         month_l = date[n]
 
+#for debugging purposes
 #print(f"highest: {highest}")
+#print(f"highest month: {month_h}")
 #print(f"lowest: {lowest}")
+#print(f"lowest month: {month_l}")
 
 #display the financial analysis in terminal
-print(f" ")
 print(f" ")
 print(f" ")
 print(f"Financial Analysis")
 print(f"-------------------------------")
 print(f"Total Months: {months}")
-print(f"Total: ${totalprof}")
-print(f"Average Change: ${(totalchange/(months - 1))}")
-print(f"Greatest Increase in Profits: {month_h} (${highest})")
-print(f"Greatest Decrease in Profits: {month_l} (${lowest})")
+print(f"Total: ${int(totalprof)}")
+print(f"Average Change: ${round((totalchange/(months - 1)), 2)}")
+print(f"Greatest Increase in Profits: {month_h} (${int(highest)})")
+print(f"Greatest Decrease in Profits: {month_l} (${int(lowest)})")
 
-#output the financial analysis to file in the Analysis folder
-
+#output the analysis.txt file to the Analysis folder
+with open("Analysis/Analysis.txt","w") as txtfile:
+    txtfile.write(f"Financial Analysis\n")
+    txtfile.write(f"-------------------------------\n")
+    txtfile.write(f"Total Months: {months}\n")
+    txtfile.write(f"Total: ${int(totalprof)}\n")
+    txtfile.write(f"Average Change: ${round((totalchange/(months - 1)), 2)}\n")
+    txtfile.write(f"Greatest Increase in Profits: {month_h} (${int(highest)})\n")
+    txtfile.write(f"Greatest Decrease in Profits: {month_l} (${int(lowest)})\n")
