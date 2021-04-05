ae925d6d126001f04702f278edd417a0da8709f7
Scott Cerny <scottcerny@yahoo.com>
2021-04-05 12:20:09 -0700

    overhauled the code since prior attempt was not calculating actual month to month change

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -4,87 +4,73 @@ import csv
 # Set path for file
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-# create places to store data
+# create lists to store data
+# set months to zero to start
+
+months = 0
 date = []
 profloss = []
 
 
-# with open(udemy_csv, newline='', encoding='utf-8') as csvfile:
+# read in the csv file
 with open(csvpath, newline='', encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     csv_header = next(csvreader)
 #    print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header and append to list
+# Read each row of data after the header and append to list while incrementing the month count
     for row in csvreader:
-    	date.append(row[0])
-#    	print(row[0])
-    	profloss.append(row[1])
-#    	print(row[1])
-#    	total = sum(int(row[1])
+        months += 1
+        #print(row)
+        date.append(row[0])
+        profloss.append(float(row[1]))
+        
+#print(date)
+#print(profloss)
+#set totalprof to the first profloss number
+totalprof = profloss[0]
+#set variables to zero
+totalchange = 0
+highest = 0
+lowest = 0
 
+#print(profloss[0])
+#print(f" total prof:  {totalprof}")
 
+#start at 1 since we already grabbed 0 to determine the change each month
 
-#    print(total)
-#    print(date)
-#    print(profloss)
-#    print(len(date))
+for n in range (1, months):
+    #calculate total amount of profit/loss
+    totalprof += profloss[n]
+    
+    #calculate average change
+    change = profloss[n] - profloss[n-1]
+    totalchange += change
+    
+    #calculate greatest increase
+    if change > highest:
+        highest = change
+        month_h = date[n]
+    #calculate greatest decrease    
+    elif change < lowest:
+        lowest = change
+        month_l = date[n]
 
-#    for amount in profloss:
-#    	total = int(sum(amount))
+#print(f"highest: {highest}")
+#print(f"lowest: {lowest}")
 
-#total = int([6]) / int(row[5])
-#        review_percent.append(percent)
-
-total = [int(i) for i in profloss]
-#average = sum(total)/len(profloss)
-#avg2 = round(average, 2)
-
+#display the financial analysis in terminal
 print(f" ")
 print(f" ")
 print(f" ")
 print(f"Financial Analysis")
 print(f"-------------------------------")
-print(f"Total Months: {len(date)}")
-print(f"Total: ${sum(total)}")
-print(f"Average Change: ${round((sum(total)/len(profloss)), 2)}")
-#print(f"Average Change: ${avg2}")
-print(f"Greatest Increase in Profits: ")
-print(f"Greatest Decrease in Profits: ")
+print(f"Total Months: {months}")
+print(f"Total: ${totalprof}")
+print(f"Average Change: ${(totalchange/(months - 1))}")
+print(f"Greatest Increase in Profits: {month_h} (${highest})")
+print(f"Greatest Decrease in Profits: {month_l} (${lowest})")
 
+#output the financial analysis to file in the Analysis folder
 
-#print(sum(total))
-
-#    	date.append(row[0])
-#    	profloss.append(row[1])
-
-#cleaned_csv = zip(date, profloss)
-
-#print(cleaned_csv)
-
-#for row in cleaned_csv:
-#	print(row)
-
-
-
-
-#rows = len(group1)
-#columns = len(group1[0])
-
-#calculate total number of months
-
-
-#calculate total amount of profit/loss
-
-
-#calculate average change
-
-
-#calculate greatest increase
-
-
-#calculate greatest decrease
-
-
-#export to text file in Analysis folder
